52fbf31c82fae305be64ca7e402be28856e85f29
Britt Treece <britt.treece@gmail.com>
2019-04-05 15:06:39 -0500

    Update network structs with pointers

--- rest/network.go
+++ rest/network.go
@@ -7,39 +7,39 @@ import (
 )
 
 type Network struct {
-	ID                string      `json:"id,omitempty"`
-	Type              string      `json:"type,omitempty"`
-	Clock             int64       `json:"clock,omitempty"`
-	UI                UI          `json:"ui,omitempty"`
-	Config            Config      `json:"config,omitempty"`
-	Description       string      `json:"description,omitempty"`
-	OnlineMemberCount int64       `json:"onlineMemberCount,omitempty"`
-	Permissions       Permissions `json:"permissions,omitempty"`
-	RulesSource       string      `json:"rulesSource,omitempty"`
-	TagsByName        TagsByName  `json:"tagsByName,omitempty"`
+	ID                string       `json:"id,omitempty"`
+	Type              string       `json:"type,omitempty"`
+	Clock             int64        `json:"clock,omitempty"`
+	UI                *UI          `json:"ui,omitempty"`
+	Config            *Config      `json:"config,omitempty"`
+	Description       string       `json:"description,omitempty"`
+	OnlineMemberCount int64        `json:"onlineMemberCount,omitempty"`
+	Permissions       *Permissions `json:"permissions,omitempty"`
+	RulesSource       string       `json:"rulesSource,omitempty"`
+	TagsByName        *TagsByName  `json:"tagsByName,omitempty"`
 }
 
 type Config struct {
-	ActiveMemberCount     int64        `json:"activeMemberCount,omitempty"`
-	AuthTokens            []string     `json:"authTokens,omitempty"`
-	AuthorizedMemberCount int64        `json:"authorizedMemberCount,omitempty"`
-	Capabilities          []string     `json:"capabilities,omitempty"`
-	Clock                 int64        `json:"clock,omitempty"`
-	CreationTime          int64        `json:"creationTime,omitempty"`
-	ID                    string       `json:"id,omitempty"`
-	LastModified          int64        `json:"lastModified,omitempty"`
-	MulticastLimit        int64        `json:"multicastLimit,omitempty"`
-	Name                  string       `json:"name,omitempty"`
-	Nwid                  string       `json:"nwid,omitempty"`
-	Objtype               string       `json:"objtype,omitempty"`
-	Private               bool         `json:"private,omitempty"`
-	Revision              int64        `json:"revision,omitempty"`
-	Routes                []Routes     `json:"routes,omitempty"`
-	Rules                 []Rules      `json:"rules,omitempty"`
-	Tags                  []string     `json:"tags,omitempty"`
-	TotalMemberCount      int64        `json:"totalMemberCount,omitempty"`
-	V4AssignMode          V4AssignMode `json:"v4AssignMode,omitempty"`
-	V6AssignMode          V6AssignMode `json:"v6AssignMode,omitempty"`
+	ActiveMemberCount     int64         `json:"activeMemberCount,omitempty"`
+	AuthTokens            []string      `json:"authTokens,omitempty"`
+	AuthorizedMemberCount int64         `json:"authorizedMemberCount,omitempty"`
+	Capabilities          []string      `json:"capabilities,omitempty"`
+	Clock                 int64         `json:"clock,omitempty"`
+	CreationTime          int64         `json:"creationTime,omitempty"`
+	ID                    string        `json:"id,omitempty"`
+	LastModified          int64         `json:"lastModified,omitempty"`
+	MulticastLimit        int64         `json:"multicastLimit,omitempty"`
+	Name                  string        `json:"name,omitempty"`
+	Nwid                  string        `json:"nwid,omitempty"`
+	Objtype               string        `json:"objtype,omitempty"`
+	Private               bool          `json:"private,omitempty"`
+	Revision              int64         `json:"revision,omitempty"`
+	Routes                []*Routes     `json:"routes,omitempty"`
+	Rules                 []*Rules      `json:"rules,omitempty"`
+	Tags                  []string      `json:"tags,omitempty"`
+	TotalMemberCount      int64         `json:"totalMemberCount,omitempty"`
+	V4AssignMode          *V4AssignMode `json:"v4AssignMode,omitempty"`
+	V6AssignMode          *V6AssignMode `json:"v6AssignMode,omitempty"`
 }
 
 type Routes struct {
@@ -55,25 +55,25 @@ type Rules struct {
 }
 
 type V4AssignMode struct {
-	Properties Properties `json:"properties,omitempty"`
+	Properties *Properties `json:"properties,omitempty"`
 }
 
 type V6AssignMode struct {
-	Properties Properties `json:"properties,omitempty"`
+	Properties *Properties `json:"properties,omitempty"`
 }
 
 type UI struct {
-	Properties Properties `json:"properties,omitempty"`
+	Properties *Properties `json:"properties,omitempty"`
 }
 
 type TagsByName struct {
-	Properties Properties `json:"properties,omitempty"`
+	Properties *Properties `json:"properties,omitempty"`
 }
 
 type Properties struct{}
 
 type Permissions struct {
-	ID ID `json:"{id},omitempty"`
+	ID *ID `json:"{id},omitempty"`
 }
 
 type ID struct {
